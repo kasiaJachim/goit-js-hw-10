26d5cbffa83888f306423548b4a08921e99305aa
kasiaJachim <kasiajachimowska66@gmail.com>
2023-05-12 11:54:07 +0100

    fetch file

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,9 +1,10 @@
-export async function fetchCountries(name) {
-    const response = await fetch(
-        `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
-    );
-    if (!response.ok) {
+export function fetchCountries(name) {
+    return fetch(
+      `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
+    ).then(response => {
+      if (!response.ok) {
         throw new Error(response.status);
-    }
-    return await response.json();
+      }
+      return response.json();
+    });
   }
